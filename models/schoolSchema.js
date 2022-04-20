4ca6f961ce8f8f0cd16293a238d7d6fa6bf49175
Suglo Suleman Rufiatu <101270194+rufia-suleman@users.noreply.github.com>
2022-04-20 08:05:00 -0700

    first commit

---
+++ models/schoolSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const schoolScheman = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    level:{
+        type:String,
+        required: true
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    numOfStudents:{
+        type:Number,
+        required: true
+    },
+    location:{
+        type:String,
+        required: true
+    },
+    address:{
+        gps:String,
+        box: String
+    },
+    contact:{
+        phone:String,
+        fax: String,
+        email:String
+    }
+},{
+    timestamps:true
+}
+)
+
+const School = mongoose.model("School", schoolScheman)
+module.exports = School;
